18da904e5d5068aa8ebabe3f3575dc97a2e60e9d
Ficram Manifur Farissa <ficramm@gmail.com>
2026-08-24 21:31:42 +0700

    Create TimerModule.h

---
+++ 06-program-architecture/02-modular-programming/TimerModule.h
@@ -0,0 +1,60 @@
+#ifndef TIMER_MODULE_H
+#define TIMER_MODULE_H
+
+#include <Arduino.h>
+
+class TimerModule {
+private:
+  struct Timer {
+    unsigned long interval;
+    unsigned long lastTrigger;
+    void (*callback)();
+    bool active;
+  };
+  
+  static const int MAX_TIMERS = 5;
+  static Timer timers[MAX_TIMERS];
+  static int timerCount;
+  
+public:
+  static void init() {
+    timerCount = 0;
+    for (int i = 0; i < MAX_TIMERS; i++) {
+      timers[i].active = false;
+    }
+  }
+  
+  static bool setInterval(unsigned long interval, void (*callback)()) {
+    if (timerCount >= MAX_TIMERS) {
+      return false;
+    }
+    
+    timers[timerCount].interval = interval;
+    timers[timerCount].lastTrigger = millis();
+    timers[timerCount].callback = callback;
+    timers[timerCount].active = true;
+    timerCount++;
+    
+    return true;
+  }
+  
+  static void update() {
+    unsigned long currentTime = millis();
+    
+    for (int i = 0; i < timerCount; i++) {
+      if (timers[i].active) {
+        if (currentTime - timers[i].lastTrigger >= timers[i].interval) {
+          timers[i].lastTrigger = currentTime;
+          if (timers[i].callback != NULL) {
+            timers[i].callback();
+          }
+        }
+      }
+    }
+  }
+};
+
+TimerModule::Timer TimerModule::timers[MAX_TIMERS];
+int TimerModule::timerCount = 0;
+
+#endif
