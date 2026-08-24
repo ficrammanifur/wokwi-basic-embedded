9f69bbd7a830fb921b3321001c12bded1e405a9a
Ficram Manifur Farissa <ficramm@gmail.com>
2026-08-25 00:48:42 +0700

    Update sketch.ino

--- 02-communication/02-i2c/sketch.ino
+++ 02-communication/02-i2c/sketch.ino
@@ -1,99 +1,131 @@
 /*
  * I2C - I2C Communication
- * 
- * Konsep: Komunikasi I2C dengan sensor suhu/kelembaban
- * 
+ *
+ * Konsep:
+ * Komunikasi I2C antara ESP32 dan LCD 16x2
+ *
  * Komponen:
- * - AHT20 Temperature & Humidity Sensor
- * 
+ * - ESP32
+ * - LCD 16x2 I2C
+ *
  * Pinout:
  * - SDA -> GPIO 21
  * - SCL -> GPIO 22
+ *
+ * Alamat I2C:
+ * - Umumnya 0x27 atau 0x3F
  */
 
 #include <Wire.h>
+#include <LiquidCrystal_I2C.h>
 
-// I2C pins
+// ==============================
+// I2C PIN
+// ==============================
+
 #define I2C_SDA 21
 #define I2C_SCL 22
 
-// AHT20 address
-#define AHT20_ADDR 0x38
+// Alamat LCD I2C
+#define LCD_ADDRESS 0x27
+
+// LCD 16 kolom x 2 baris
+LiquidCrystal_I2C lcd(LCD_ADDRESS, 16, 2);
+
+// ==============================
+// SETUP
+// ==============================
 
 void setup() {
+
   Serial.begin(115200);
-  
+
   // Inisialisasi I2C
   Wire.begin(I2C_SDA, I2C_SCL);
-  Wire.setClock(100000); // 100kHz
-  
+  Wire.setClock(100000); // 100 kHz
+
   Serial.println("I2C Demo Started");
-  
-  // Scan I2C devices
+  Serial.println("================");
+
+  // Scan perangkat I2C
   scanI2C();
-  
-  // Initialize AHT20
-  initAHT20();
+
+  // Inisialisasi LCD
+  lcd.init();
+  lcd.backlight();
+
+  // Tampilkan pesan
+  lcd.setCursor(0, 0);
+  lcd.print("I2C Demo");
+
+  lcd.setCursor(0, 1);
+  lcd.print("ESP32 + LCD");
 }
+
+// ==============================
+// LOOP
+// ==============================
 
 void loop() {
-  // Read temperature and humidity
-  float temp, hum;
-  if (readAHT20(temp, hum)) {
-    Serial.print("Temperature: ");
-    Serial.print(temp);
-    Serial.print(" °C | Humidity: ");
-    Serial.print(hum);
-    Serial.println(" %");
-  } else {
-    Serial.println("Failed to read AHT20");
-  }
-  
-  delay(2000);
+
+  // Contoh komunikasi/data yang berubah
+  static int counter = 0;
+
+  lcd.setCursor(0, 0);
+  lcd.print("I2C Communication");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Count: ");
+  lcd.print(counter);
+
+  // Bersihkan sisa karakter
+  lcd.print("        ");
+
+  Serial.print("Counter: ");
+  Serial.println(counter);
+
+  counter++;
+
+  delay(1000);
 }
+
+// ==============================
+// I2C SCANNER
+// ==============================
 
 void scanI2C() {
+
   Serial.println("Scanning I2C devices...");
+
+  byte deviceCount = 0;
+
   for (byte address = 1; address < 127; address++) {
+
     Wire.beginTransmission(address);
-    if (Wire.endTransmission() == 0) {
-      Serial.print("Found device at 0x");
+
+    byte error = Wire.endTransmission();
+
+    if (error == 0) {
+
+      Serial.print("Found I2C device at 0x");
+
+      if (address < 16) {
+        Serial.print("0");
+      }
+
       Serial.println(address, HEX);
+
+      deviceCount++;
     }
   }
-}
 
-void initAHT20() {
-  Wire.beginTransmission(AHT20_ADDR);
-  Wire.write(0xBE); // Init command
-  Wire.write(0x08);
-  Wire.write(0x00);
-  Wire.endTransmission();
-  delay(100);
-}
-
-bool readAHT20(float& temp, float& hum) {
-  Wire.beginTransmission(AHT20_ADDR);
-  Wire.write(0xAC); // Trigger measurement
-  Wire.write(0x33);
-  Wire.write(0x00);
-  Wire.endTransmission();
-  delay(80);
-  
-  Wire.requestFrom(AHT20_ADDR, 6);
-  if (Wire.available() == 6) {
-    byte data[6];
-    for (int i = 0; i < 6; i++) {
-      data[i] = Wire.read();
-    }
-    
-    uint32_t humRaw = ((uint32_t)data[1] << 12) | ((uint32_t)data[2] << 4) | (data[3] >> 4);
-    uint32_t tempRaw = ((uint32_t)(data[3] & 0x0F) << 16) | ((uint32_t)data[4] << 8) | data[5];
-    
-    hum = humRaw * 100.0 / 1048576.0;
-    temp = tempRaw * 200.0 / 1048576.0 - 50.0;
-    
-    return true;
+  if (deviceCount == 0) {
+    Serial.println("No I2C devices found!");
+  } else {
+    Serial.print("Found ");
+    Serial.print(deviceCount);
+    Serial.println(" I2C device(s).");
   }
-  return false;
+
+  Serial.println("================");
 }
